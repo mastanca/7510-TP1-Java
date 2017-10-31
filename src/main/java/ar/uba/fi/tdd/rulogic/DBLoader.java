c1dc03d8e9c1c542ad2b48129632e816944c79cf
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-10-31 15:07:06 -0300

    Normalize db

--- src/main/java/ar/uba/fi/tdd/rulogic/DBLoader.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/DBLoader.java
@@ -3,15 +3,24 @@ package ar.uba.fi.tdd.rulogic;
 import ar.uba.fi.tdd.rulogic.model.Fact;
 import ar.uba.fi.tdd.rulogic.model.Rule;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DBLoader {
     private List facts;
     private List rules;
 
-    public DBLoader() {
-        this.facts = new ArrayList<Fact>();
+    public DBLoader(String filename) throws IOException, URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        Stream<String> data = Files.lines(Paths.get(ClassLoader.getSystemClassLoader()
+                .getResource(filename).toURI()));
+        this.facts = data.filter(str -> !str.contains(":-")).map(Fact::new).collect(Collectors.toList());
         this.rules = new ArrayList<Rule>();
     }
 
@@ -21,5 +30,15 @@ public class DBLoader {
 
     public List getRules() {
         return rules;
+    }
+
+    public String normalize(String line) {
+        return line.replaceAll("\\s\\(\\s", "(").
+                replaceAll("\\s\\)\\s", ")").
+                replaceAll("\\)\\s*\\.$", "\\)\\.").
+                replaceAll("\\t", " ").
+                replaceAll("[,!?;]", "$0 ").replaceAll("\\s+", " ").
+                replaceAll("\\):", "\\) :")
+                .trim();
     }
 }
--- src/test/java/DBLoaderTest.java
+++ src/test/java/DBLoaderTest.java
@@ -2,11 +2,27 @@ import ar.uba.fi.tdd.rulogic.DBLoader;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class DBLoaderTest {
-    private DBLoader dbLoader = new DBLoader();
+    private DBLoader dbLoader = new DBLoader("rules.db");
+
+    public DBLoaderTest() throws IOException, URISyntaxException {
+    }
 
     @Test
     public void testLoadFromFile() {
-        //Assert.assertTrue(dbLoader.getFacts().size(), 1);
+        Assert.assertEquals(dbLoader.getFacts().size(), 15);
+    }
+
+    @Test
+    public void testNormalizeDatabase() {
+        String str1 = "varon ( nicolas ) .";
+        Assert.assertEquals("varon(nicolas).", dbLoader.normalize(str1));
+        String str2 = "mujer(maria) .";
+        Assert.assertEquals("mujer(maria).", dbLoader.normalize(str2));
+        String str3 = "tio(X, Y, Z):- varon(X),\thermano(X, Z),padre(Z, Y).\n";
+        Assert.assertEquals("tio(X, Y, Z) :- varon(X), hermano(X, Z), padre(Z, Y).", dbLoader.normalize(str3));
     }
 }
